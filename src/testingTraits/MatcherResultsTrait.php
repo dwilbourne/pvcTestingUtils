8e2578f4863e3880b1c0bb372687fd12c4096b1c
Doug Wilbourne <dougwilbourne@gmail.com>
2025-11-27 12:35:03 -0500

    added makeMockReturnFromResultsArray trait

--- src/testingTraits/MatcherResultsTrait.php
+++ src/testingTraits/MatcherResultsTrait.php
@@ -9,11 +9,38 @@ use PHPUnit\Framework\TestCase;
 
 trait MatcherResultsTrait
 {
-    public function makeMockReturnOnConsecutiveInvocations(MockObject $mock, string $method, array $results): void
+    /**
+     * makeMockReturnOnConsecutiveInvocations
+     * @param  MockObject  $mock
+     * @param  string  $method
+     * @param  array<int, mixed>  $results
+     *
+     * @return void
+     */
+    public function makeMockReturnByInvocationNumber(MockObject $mock, string $method, array $results): void
     {
         $matcher = TestCase::exactly(count($results));
         $closure = function () use ($matcher, $results) {
             return $results[$matcher->numberOfInvocations()];
+        };
+        $mock->expects($matcher)->method($method)->willReturnCallback($closure);
+    }
+
+    /**
+     * makeMockReturnFromResultsArray
+     * @param  MockObject  $mock
+     * @param  string  $method
+     * @param  array<int|string, mixed>  $results
+     *
+     * @return void
+     */
+    public function makeMockReturnFromResultsArray(MockObject $mock, string $method, array $results): void
+    {
+        $matcher = TestCase::exactly(count($results));
+        $args = array_keys($results);
+        $closure = function ($index) use ($args, $results) {
+            $key = $args[$index];
+            return $results[$key];
         };
         $mock->expects($matcher)->method($method)->willReturn($closure);
     }
